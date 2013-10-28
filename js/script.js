83ea6ca7d853bd9efb380341bfa678b9cedca18d
Felix Wang <wayjaywang@gmail.com>
2013-10-28 18:23:14 -0400

    Oct 28 WorkOf Client Requests
    Random Tagline generated on page load
    switched slideshow order
    swapped maker images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function(){
   $("#slideshow > div:gt(0)").hide();
 
@@ -10,10 +11,33 @@ $(document).ready(function(){
       .end()
       .appendTo('#slideshow');
   },  4000);
+
+  var quotes = new Array();
+  quotes[0] = "Connecting the best New York City Makers to Interior Designers";
+  quotes[1] = "Connecting Independent Brooklyn Makers to the NYC Design Community ";
+  quotes[2] = "We connect New York City Furniture Makers to the local Interior Design Community";
+  var rando = quotes[Math.floor( Math.random() * quotes.length )];
+  $('.hero-text').text(rando);
+
+//write something to the console
+
+//write one headline "string" to the console
+
+//write all the headlines to the console
+
+//write all the headlines in an array to the console
+
+//write one string within the array to the console
+
+//write one random string within the array to the console
+
+//write one random string within the array to the h1
 });
+
 $(function(){
     $('.js-scroll-to-invite').click(function(){
         $('html,body').animate({ scrollTop: $('.signup').offset().top }, 1000);
         $('#email-input-field').focus();
     });
 });
+
