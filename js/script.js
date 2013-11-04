3d3732e385ab47fdfee3c6a13eec32c646e8426a
Felix Wang <wayjaywang@gmail.com>
2013-11-04 11:17:53 -0500

    WorkOf Client Presentation for Nov 1 2013

--- js/script.js
+++ js/script.js
@@ -13,13 +13,6 @@ $(document).ready(function(){
       .appendTo('#slideshow');
   },  4000);
 
-  var quotes = new Array();
-  quotes[0] = "Connecting the best New York City Makers to Interior Designers";
-  quotes[1] = "Connecting Independent Brooklyn Makers to the NYC Design Community ";
-  quotes[2] = "We connect New York City Furniture Makers to the local Interior Design Community";
-  var rando = quotes[Math.floor( Math.random() * quotes.length )];
-  $('.hero-text').text(rando);
-
 });
 
 $(function(){
@@ -29,3 +22,29 @@ $(function(){
     });
 });
 
+
+$(document).ready(function(){
+// show popup when you click on the link
+  $('.show-popup').click(function(event){
+    event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+    $('.overlay-bg').fadeIn(200); //display your popup
+    $('body').addClass('stop-scrolling');
+  });
+   
+  // hide popup when user clicks on close button
+  $('.close-btn').click(function(){
+    $('.overlay-bg').fadeOut(200);
+    $('body').removeClass('stop-scrolling');// hide the overlay
+  });
+   
+  // hides the popup if user clicks anywhere outside the container
+  $('.overlay-bg').click(function(){
+      $('.overlay-bg').fadeOut(200);
+      $('body').removeClass('stop-scrolling');
+  });
+  // prevents the overlay from closing if user clicks inside the popup overlay
+  $('.overlay-content').click(function(){
+      return false;
+  });
+ 
+});
